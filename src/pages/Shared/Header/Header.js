aa8764403704eb613de6b40cfacbefa4a6fea272
arifa-akter <arifaakterchoity@gmail.com>
2022-04-17 15:43:41 +0600

    add login in header

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import logo2 from '../../../images/logo2.png'
+import profile from '../../../images/profile.png'
 import './Header.css'
-import logo from '../../../images/logo.jpg'
+
 
 const Header = () => {
     return (
         <section className='nav-style'>
+          <section className='login-signUp'>
+            <div className="container">
+               <div className="row">
+                 <div className="col-4 col-lg-4">
+                   <div className="logo-image">
+                   <img src={logo2} alt="" />
+                   </div>
+                 </div>
+                  <div className="col-8 col-lg-8 d-flex justify-content-end align-items-center "> 
+                     <img className='link-image' src={profile} alt="" />
+                     <Link className='link-login' to ="/login">Login</Link>
+                      <Link  className='link-login' to ="/signup">sign up</Link>
+                  </div>
+               </div>
+            </div>
+          </section>
         <Navbar collapseOnSelect expand="lg"  variant="dark">
           <Container>
           <Navbar.Brand as={Link} to="">
-              <img style={{width:'50px', height:'50px'}} src={logo} alt="" />
               <span className="menu-title">WILD PHOTOGRAPHER </span>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -23,12 +40,6 @@ const Header = () => {
               <Nav.Link href="#deets"><span className="menu">Gallery</span></Nav.Link>
               <Nav.Link as={Link} to="" href="home#rooms" >
               <span className="menu">Blogs</span>
-              </Nav.Link>
-              <Nav.Link as={Link} to="/login">
-              <span className="menu">Login</span>
-              </Nav.Link>
-              <Nav.Link as={Link} to="/signUp">
-              <span className="menu">signUp</span>
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
