1955994a9a7026f18c6e88cb5bf0ccda13661ddf
arifa-akter <arifaakterchoity@gmail.com>
2022-04-17 06:19:58 +0600

    add header section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home/Home';
+import Header from './pages/Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
      
     </div>
   );
---
+++ src/pages/Shared/Header/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css'
+import logo from '../../../images/logo.jpg'
+
+const Header = () => {
+    return (
+        <section className='nav-style'>
+        <Navbar collapseOnSelect expand="lg"  variant="dark">
+          <Container>
+          <Navbar.Brand as={Link} to="/home">
+              <img style={{width:'50px', height:'50px'}} src={logo} alt="" />
+              <span className="menu-title">TEN STAR HOTEL </span>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/home"><span className="menu">Home</span></Nav.Link>
+              <Nav.Link as={Link} to="/about"><span className="menu">About</span></Nav.Link>
+        
+              <Nav.Link href="#deets"><span className="menu">Service</span></Nav.Link>
+              <Nav.Link href="#deets"><span className="menu">Gallery</span></Nav.Link>
+              <Nav.Link as={Link} to="/roomDetail/home#rooms" href="home#rooms" >
+              <span className="menu">Blogs</span>
+              </Nav.Link>
+              <Nav.Link as={Link} to="/login">
+              <span className="menu">Login</span>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        </section>
+    );
+};
+
+export default Header;
